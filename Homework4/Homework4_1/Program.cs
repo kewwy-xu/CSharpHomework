f163fd7e856829406592d2703427b1347166ef8c
徐汝明 <2229807049@qq.com>
2020-03-17 15:40:52 +0800

    第四次作业

---
+++ Homework4/Homework4_1/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Homework4_1
+{
+    // 链表节点
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    //泛型链表类
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action<T> action)
+        {
+            Node<T> t = this.head;
+            while(t!=null)
+            {
+                action(t.Data);
+                t = t.Next;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> aList = new GenericList<int>();
+            for(int i=0;i<10;i++)
+            {
+                aList.Add(i);
+            }
+            //打印每个链表元素
+            aList.ForEach(m => Console.WriteLine(m));
+            //求最大值
+            int max = aList.Head.Data;
+            aList.ForEach(m => { if (max < m) max = m; });
+            Console.WriteLine("最大值为：" + max);
+            //求最小值
+            int min = aList.Head.Data;
+            aList.ForEach(m => { if (min > m) min = m; });
+            Console.WriteLine("最小值为：" + min);
+            //求和
+            int sum = 0;
+            aList.ForEach(m => sum += m);
+            Console.WriteLine("和为：" + sum);
+        }
+    }
+}
---
+++ Homework4/Homework4_2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Homework4_2
+{
+    public delegate void ClockAlarm(object sender);  //声明委托
+    public class Clock
+    {
+        public event ClockAlarm Alarm;   //定义响铃事件，相当于创建一个委托实例
+        public void SetClock(string h,string m,string s)   //设置响铃时间
+        {
+            while (true)
+            {
+                //打印当前时间
+                Console.WriteLine(DateTime.Now.ToLongTimeString().ToString());   
+                System.Threading.Thread.Sleep(1000);
+
+                //判断当前时间是否等于响铃时间
+                if (h == DateTime.Now.Hour.ToString() && m == DateTime.Now.Minute.ToString()
+                    && s == DateTime.Now.Second.ToString())
+                {
+                    Alarm(this);    //触发响铃事件
+                }
+            }
+        }
+    }
+    public class RunClock
+    {
+        public Clock clock = new Clock();
+        //为Alarm事件添加一个处理方法;响铃CAlarm()
+        public RunClock()
+        {
+            clock.Alarm += CAlarm;
+        }     
+        public void CAlarm(object sender)
+        {
+            while(true)
+            {
+                Console.WriteLine("叮叮叮-------------------叮叮叮");
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunClock runClock =new RunClock();
+            runClock.clock.SetClock("15","37","45");
+        }
+    }
+}
