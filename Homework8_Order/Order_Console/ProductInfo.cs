ba967a6919f3ea75ea0f9df40c0ee9534bd10c44
徐汝明 <2229807049@qq.com>
2020-04-12 08:54:57 +0800

    第八次作业，WinForm订单管理系统

---
+++ Homework8_Order/Order_Console/ProductInfo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order_Console
+{
+    public class ProductInfo
+    {
+        public static Dictionary<string, string> NameNoDic
+        {
+            get{
+                Dictionary<string, string> nameNoDic = new Dictionary<string, string>
+                {
+                    { "篮球", "001" },
+                    { "足球", "002" },
+                    { "乒乓球", "003" },
+                    { "排球", "004" },
+                    { "羽毛球", "005" },
+                    { "棒球", "006" },
+                    { "网球", "007" }
+                };
+                return nameNoDic;
+            }
+        }
+        public static Dictionary<string, float> NamePriceDic
+        {
+            get
+            {
+                Dictionary<string, float> namePriceDic = new Dictionary<string, float>
+                {
+                    { "篮球", 200 },
+                    { "足球", 145 },
+                    { "乒乓球", 5 },
+                    { "排球", 123.5f },
+                    { "羽毛球", 3.99f },
+                    { "棒球", 86 },
+                    { "网球", 98.3f }
+                };
+                return namePriceDic;
+            }
+        }
+    }
+}
---
+++ Homework8_Order/Order_Winform/MainForm.cs
@@ -0,0 +1,108 @@
+﻿using Order_Console;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Order_Winform
+{
+    public partial class MainForm : Form
+    {
+        public string KeyWord { get; set; }
+        public static List<Order> orderList = new List<Order>();
+        public static OrderService orderService = new OrderService(orderList);
+        public MainForm()
+        {
+            InitializeComponent();
+
+            List<OrderItem> orderItems1 = new List<OrderItem>()
+            {  new OrderItem( "篮球",  3),
+               new OrderItem("足球",2),
+               new OrderItem("排球",5),
+            };
+            List<OrderItem> orderItems2 = new List<OrderItem>()
+            {
+               new OrderItem("足球",1),
+               new OrderItem("乒乓球",10)
+            };
+
+            orderService.AddOrder(new Order("00000001", new DateTime(2020, 3, 21, 14, 20, 00), "徐先生", orderItems1));
+            orderService.AddOrder(new Order("00000002", new DateTime(2020, 3, 20, 9, 50, 00), "刘女士", orderItems2));                   
+
+            order_bindingSource.DataSource = orderService.orderList;
+           
+            queryInput_textbox.DataBindings.Add("Text", this, "KeyWord");
+        }
+
+        //按订单号查询
+        private void queryByCustomerName_btn_Click(object sender, EventArgs e)
+        {
+            if (KeyWord == null || KeyWord == "")
+            {
+                order_bindingSource.DataSource = orderList;
+            }
+            else
+            {
+                order_bindingSource.DataSource =
+                    orderList.Where(s => s.customerName == KeyWord);
+            }
+        }
+
+        //按客户名查询
+        private void queryByOrderNo_btn_Click(object sender, EventArgs e)
+        {
+            if (KeyWord == null || KeyWord == "")
+            {
+                order_bindingSource.DataSource = orderList;
+            }
+            else
+            {
+                order_bindingSource.DataSource =
+                   orderList.Where(s => s.orderNo == KeyWord);
+            }
+        }
+
+        //添加订单
+        private void addOrder_btn_Click(object sender, EventArgs e)
+        {
+            AddOrderForm add = new AddOrderForm();   
+            add.ShowDialog();
+        }
+
+        //导入订单
+        private void importOrder_btn_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                orderService.ImportOrders(openFileDialog1.FileName);
+            }
+        }
+
+        //导出订单
+        private void exportOrder_btn_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                orderService.ExportAllOrders(openFileDialog1.FileName);
+            }           
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bindingNavigatorDeleteItem_Click(object sender, EventArgs e)
+        {
+            foreach(Order o in orderList)
+            {
+                Console.WriteLine(o.orderNo);
+            }
+        }
+    }
+}
